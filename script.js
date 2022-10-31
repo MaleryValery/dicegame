1b3c8a50e31728b5f14b283deb94c4ee424a0b4c
Valeriia Melnikov <valery.medvedeva@gmail.com>
2022-10-31 19:53:31 +0200

    feat: add toggle to players

--- script.js
+++ script.js
@@ -8,8 +8,12 @@ const diceImgEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 
 //Starting point
+let totalScores = [0, 0];
+let activePlayer = 0;
 let currentScore = 0;
 score0El.textContent = 0;
 score1El.textContent = 0;
@@ -24,8 +28,14 @@ btnRoll.addEventListener('click', function () {
     diceImgEl.src = `dice-${getDiceNumber}.png`
     //checking if dice is not equal 1
     if (getDiceNumber !== 1) {
-    //add dice to score
+    //add dice to score of active plaeyr
         currentScore += getDiceNumber;
-        currentScore0El.textContent = currentScore;
+        document.querySelector(`#current--${activePlayer}`).textContent = currentScore;
+    } else {
+        document.querySelector(`#current--${activePlayer}`).textContent = 0;
+        currentScore =0;
+        activePlayer = activePlayer === 0 ? 1 : 0;
+        player0El.classList.toggle('player--active');
+        player1El.classList.toggle('player--active');
     }
 })
